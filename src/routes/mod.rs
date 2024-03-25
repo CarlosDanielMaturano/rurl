1c49dc213d94d22c5c4f9d8b70a263262588e0dd
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-03-25 10:22:05 -0300

    feat: Implemented a route for view a single url using the shorten parameter value

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,12 @@
 mod create_shorten_url;
 mod hello_world;
-
+mod view_shorten_url;
 use rocket::Route;
 
 pub fn build_routes() -> Vec<Route> {
     routes![
         hello_world::hello_world,
-        create_shorten_url::create_shorten_url
+        create_shorten_url::create_shorten_url,
+        view_shorten_url::view_shorten_url,
     ]
 }
---
+++ src/routes/view_shorten_url.rs
@@ -0,0 +1,37 @@
+use crate::database::Db;
+use crate::responder::{ApiResponder, ApiResponse};
+use crate::models::ShortenUrl;
+use rocket::futures::TryFutureExt;
+use rocket::http::Status;
+use rocket_db_pools::{sqlx, Connection};
+use serde_json::json;
+
+#[get("/view/<shorten>")]  
+pub async fn view_shorten_url(mut db: Connection<Db>, shorten: String) -> ApiResponse {
+    let values = sqlx::query!(
+        "SELECT original, shorten FROM urls WHERE shorten = ?",
+        shorten
+    )
+    .fetch_one(&mut **db)
+    .map_err(|err| {
+        eprintln!("{err}");
+        ApiResponder::new(
+            Status::InternalServerError,         
+            json!({
+                "err": "Could not get the shorten url due to server malfunction"
+            })
+        )
+    })
+    .await?;
+    let shorten_url = ShortenUrl {
+        original_url: values.original,
+        shorten_url: values.shorten
+    };
+    Ok(ApiResponder::new(
+        Status::Ok,
+        json!({
+            "message": "Sucessfully founded the url.",
+            "shorten_url": shorten_url
+        })
+    ))
+}
