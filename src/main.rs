4872e4b774c3ae110baf2b852151ab04e225d01e
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-03-26 17:54:57 -0300

    feat: The server show a warning when the app start

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,29 @@ mod routes;
 mod catchers;
 mod logger;
 
+use log::warn;
 use rocket_db_pools::Database;
+use colored::Colorize;
 
 #[rocket::main]
 async fn main() -> Result<(), String> {
     dotenv::dotenv().ok();
     logger::setup_logger()
         .map_err(|err| format!("Could not initialize the logger. Error: {err}"))?;
-    let _rocket = rocket::build()
+    let rocket = rocket::build()
         .attach(crate::database::Db::init())
         .mount("/", routes::build_routes())
         .register("/", catchers::build_catchers())
+        .ignite()
+        .await
+        .map_err(|err| format!("Could not ignite a rocket instance Error: {err}"))?;
+
+    let port = rocket.config().port;
+    let addr = rocket.config().address;
+    let host = format!("http://{addr}:{port}").bold().underline().green();
+    warn!("Application running on {}", host);
+
+    rocket
         .launch()
         .await
         .map_err(|err| format!("Could not initalize a rocket instance. Error: {err}"))?;
