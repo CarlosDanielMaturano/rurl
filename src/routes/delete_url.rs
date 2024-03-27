e5d3b6e89a85162af9296e17cf751f056c9c89e7
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-03-27 09:15:33 -0300

    fix: Properly error return when deleting url with non-existing hash

--- src/routes/delete_url.rs
+++ src/routes/delete_url.rs
@@ -1,5 +1,5 @@
 use crate::database::Db;
-use crate::errors::{DefaultApiError, InternalServerError};
+use crate::errors::{DefaultApiError, InternalServerError, NotFoundError};
 use crate::responder::{ApiResponder, ApiResponse};
 use rocket::http::Status;
 use rocket_db_pools::{sqlx, Connection};
@@ -7,12 +7,18 @@ use serde_json::json;
 
 #[delete("/delete/<hash>")]
 pub async fn delete_url(mut db: Connection<Db>, hash: String) -> ApiResponse {
-    sqlx::query!("DELETE FROM urls where hash = ?", hash)
-        .execute(&mut **db)
+    sqlx::query!("DELETE FROM urls where hash = ? RETURNING id", hash)
+        .fetch_optional(&mut **db)
         .await
         .map_err(|err| {
             InternalServerError::new(err, "Could not delete the url due to server malfuction.")
-        })?;
+        })?
+        .ok_or_else(||
+            NotFoundError::new(
+                "Query returned none",
+                "Could not find a shorten url with the given hash",
+            )
+        )?;
 
     Ok(ApiResponder::new(
         Status::Ok,
